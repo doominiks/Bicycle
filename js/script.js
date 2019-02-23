7f1d85a7ee17ead1dd92333063b9bdb455fefb89
Dominik <dominik@MacBook-Pro-Arkadiusz.local>
2019-02-23 15:11:44 +0100

    accesories fixed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let altIcon = $('.fa-sync-alt');
 let plusIcon = $('.fa-search-plus')
 let minusIcon = $('.fa-search-minus')
-let imageBig = $('.features__image--big')
 let imageLeft = $('.intro__image--left')
 
 // Toggle Menu
@@ -28,32 +27,107 @@ if ($(window).width() > 980) {
     })
 }
 
-// Rotate, zoom in and out  Middle Bicycle
 
-function removeZoom() {
-    imageBig.elevateZoom({
-        zoomType: false,
+/* Slider made by Mamun khandaker */
+$(function () {
+
+    var liCount = $('.slider li').length;
+    var ulWidth = (liCount * 100);
+    var liWidth = (100 / liCount);
+    var leftIncrement = (ulWidth / liCount);
+
+
+    $('.slider').height($('.slider li img').height());
+
+
+    $('.slider img').on('load', function () {
+        $('.loader').fadeOut();
+        $('.slider').height($(this).height());
+    })
+
+    $(window).resize(function () {
+        $('.slider').css('height', $('.slider li img').height());
     });
-}
 
-altIcon.click(function () {
-    removeZoom();
+    $('.slider ul').css('width', ulWidth + '%');
+    $('.slider li').css('width', liWidth + '%');
 
-    imageBig.toggleClass('rotate')
-});
-
-
-plusIcon.on('click', function () {
-    imageBig.elevateZoom({
-        zoomType: "lens",
-        lensShape: "round",
-        lensSize: 220
+    $('.slider').append(function () {
+        $(this).append('<div class="navigator"></div>');
+        $(this).prepend('<span class="prev">Prev</span><span class="next">Next</span>');
+        $(this).find('li').each(function () {
+            $('.navigator').append('<span></span>');
+        });
     });
 
-    imageBig.removeClass('rotate');
+    $('.slider').css('height', $('.slider li img').height());
+
+    $('.navigator span:first-child').addClass('active');
+
+
+    if (liCount > 2) {
+        $('.slider ul').css('margin-left', -leftIncrement + '%');
+        $('.slider ul li:last-child').prependTo('.slider ul');
+    } else if (liCount == 1) {
+        $('.slider span').css('display', 'none');
+        $('.autoPlay').css('display', 'none');
+    } else if (liCount == 2) {
+        $('.slider .prev').css('display', 'none');
+    }
+
+    function moveLeft() {
+        $('.slider ul').animate({
+            left: -leftIncrement + '%'
+        }, 500, function () {
+            $('.slider ul li:first-child').appendTo('.slider ul');
+            $('.slider ul').css('left', '');
+
+            if ($('.navigator span').hasClass('active')) {
+
+                if ($('.navigator span.active').is(':last-child')) {
+                    $('span.active').removeClass('active');
+                    $('.navigator span:first-child').addClass('active');
+                } else {
+                    $('span.active').next().addClass('active');
+                    $('span.active').prev().removeClass('active');
+                }
+            }
+        });
+    }
+
+
+    function moveRight() {
+        $('.slider ul').animate({
+            left: leftIncrement + '%'
+        }, 500, function () {
+            $('.slider ul li:last-child').prependTo('.slider ul');
+            $('.slider ul').css('left', '');
+
+            if ($('.navigator span').hasClass('active')) {
+
+                if ($('.navigator span.active').is(':first-child')) {
+                    $('span.active').removeClass('active');
+                    $('.navigator span:last-child').addClass('active');
+                } else {
+                    $('span.active').prev().addClass('active');
+                    $('span.active').next().removeClass('active');
+                }
+            }
+        });
+    }
+
+    if (liCount > 1) {
+        invertalValue = setInterval(function () {
+            moveLeft();
+        }, 5000);
+    }
+
+    $('.prev').click(function () {
+        moveRight();
+    });
+
+    $('.next').click(function () {
+        moveLeft();
+    });
+
 });
-
-
-minusIcon.on('click', function () {
-    removeZoom()
-})
